0895d10c5781bb2d26786933e31371adb68804d1
yourMACARONi <sarpiuq@gmail.com>
2025-09-15 12:34:15 +0800

    Adding admin Page and disabling the inputs for Time and date

---
+++ src/presentation/pages/AdminHomePage.tsx
@@ -0,0 +1,10 @@
+import AdminNavLayout from "../layout/AdminNavLayout";
+import Home from "../components/admin/home/Home";
+
+export default function AdminHomePage() {
+  return (
+    <AdminNavLayout>
+      <Home />
+    </AdminNavLayout>
+  );
+}
